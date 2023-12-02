944b8ea38539a8e1fcb039c0c5452a34cc9dbc81
Naveenkumar <naveenbu05@gmail.com>
2023-12-02 13:00:55 +0530

    Create Daughter.java

---
+++ Code/oops/AbstracctClass/Daughter.java
@@ -0,0 +1,15 @@
+package oops.AbstracctClass;
+
+public class Daughter extends Parent {
+
+	@Override
+	void career() {
+		System.out.println("I am daughter");
+	}
+
+	@Override
+	void partner() {
+		System.out.println("I love partner ");
+	}
+
+}
